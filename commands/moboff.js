33d375bed55d1ad93c85dd459cb4214b017dc25d
TÆMBØ <kuneshr123@gmail.com>
2021-12-04 19:54:29 -0800

    this isn't pccb, change that

--- commands/moboff.js
+++ commands/moboff.js
@@ -3,6 +3,6 @@ module.exports = {
 		message.channel.send("https://cdn.discordapp.com/attachments/778848112588095559/873406468483862528/20210807_102414.gif");
 	},
 	name: 'moboff',
-	category: 'PC Creator',
+	category: 'Real Computers',
 	description: 'Shows a GIF of each motherboard form factor'
 };
