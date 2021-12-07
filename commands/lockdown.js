0eaa3e825cb5d7855e31af467d5a9b5d242a2abb
Skippy <82199824+5k1ppy@users.noreply.github.com>
2021-12-08 10:29:02 +1100

    Fixed lockdown command (will add unlock)

--- commands/lockdown.js
+++ commands/lockdown.js
@@ -3,24 +3,17 @@ module.exports = {
 		if (message.member.hasPermission('ADMINISTRATOR')) {
 
             const role = message.guild.roles.everyone;
-            const query = args[0].toLowerCase()
             const perms = role.permissions.toArray()
 
-            if (query === "end") {
-                perms.push("SEND_MESSAGES")
-                await role.edit({ permissions: perms });
-                message.channel.send('Unlocked channels')
-            } else {
-                const newPerms = perms.filter((perm) => perm !== 'SEND_MESSAGES');
-                await role.edit({ permissions: newPerms })
-                message.channel.send('Locked down channels')
-            }
+            const newPerms = perms.filter((perm) => perm !== 'SEND_MESSAGES');
+            await role.edit({ permissions: newPerms })
+            message.channel.send('Locked down channels')
+        
         } else {
-            message.reply('you do not have permission to use the lockdown command')
+            message.reply('you do not have permission to use this command')
         }
 	},
 	name: 'lockdown',
-	description: `Lock all channels that aren't moderation based (e.g. #mod-logs)`,
-	usage: ['?end'],
+	description: `Lock @ everyone send message permissions`,
 	category: 'Moderation'
 };
