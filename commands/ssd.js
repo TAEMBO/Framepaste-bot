6f1d02fb9704b36f875866a563fcb3fde78c23a3
TÆMBØ <kuneshr123@gmail.com>
2021-12-07 19:40:24 -0800

    new cmd pog

---
+++ commands/ssd.js
@@ -0,0 +1,8 @@
+module.exports = { 
+	run: (client, message, args) => {
+		message.channel.send(`https://media.discordapp.net/attachments/873064255690264596/917983590225166346/SSD_1.png`);
+	},
+	name: 'ssd',
+    category: 'Real Computers',
+	description: 'Shows all types of SSDs',
+};
