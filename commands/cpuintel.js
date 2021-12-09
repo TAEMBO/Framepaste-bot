850c581738cf33b74195255c53fcfe614c20b44f
Skippy <82199824+5k1ppy@users.noreply.github.com>
2021-12-09 17:10:45 +1100

    Delete cpuintel.js

--- commands/cpuintel.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	run: (client, message, args) => {
-		client.cpuCommand(client, message, args);
-	},
-	name: 'cpuintel',
-	description: 'Info about IRL Intel CPUs',
-	category: 'Real Computers',
-	cooldown: 7
-};
