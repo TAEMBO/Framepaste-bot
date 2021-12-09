80917ecc1228fdba91100d8abb5614ded111596d
Skippy <82199824+5k1ppy@users.noreply.github.com>
2021-12-09 17:10:39 +1100

    Delete cpuamd.js

--- commands/cpuamd.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	run: (client, message, args) => {
-		client.cpuCommand(client, message, args);
-	},
-	name: 'cpuamd',
-	description: 'Info about IRL AMD CPUs',
-	category: 'Real Computers',
-	cooldown: 7
-};
