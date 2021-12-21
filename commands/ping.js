1edf4b12e84417a67e578df32bb2317250baab2d
TÆMBØ <kuneshr123@gmail.com>
2021-12-20 18:58:08 -0800

    first fancy reply

--- commands/ping.js
+++ commands/ping.js
@@ -1,6 +1,6 @@
 module.exports = { 
 	run: (client, message, args) => {
-		message.channel.send(`WebSocket Latency: **${client.ws.ping}ms**`);
+		message.reply(`WebSocket Latency: **${client.ws.ping}ms**`);
 	},
 	name: 'ping',
 	description: 'Average latency of bot client\'s shards\' last websocket heartbeat from sending to ack in milliseconds. No it\'s not _your_ ping.',
