d1d74ef5e156adfd78865acd2655064b8a1a0268
TÆMBØ <kuneshr123@gmail.com>
2021-12-21 16:55:54 -0800

    update banned words

--- index.js
+++ index.js
@@ -776,7 +776,7 @@ client.on("messageCreate", async (message) => {
 	}
 
 	// handle banned words
-	const bannedWords = ["shlt", "fů", "cum", "shit", " ass ", "fuck", "nigg", "fuk", "cunt", "cnut", "bitch", " dick", "dick ", "d1ck", "pussy", "asshole", "b1tch", "b!tch", "blowjob", "cock", "c0ck", "retard", " fag", "fag ", "faggot"]
+	const bannedWords = ["f u c k", "shlt", "fů", "cum", "shit", " ass ", "fuck", "nigg", "fuk", "cunt", "cnut", "bitch", " dick", "dick ", "d1ck", "pussy", "asshole", "b1tch", "b!tch", "blowjob", "cock", "c0ck", "retard", " fag", "fag ", "faggot"]
 	
 	if (bannedWords.some(word => message.content.toLowerCase().includes(word)) && !client.hasModPerms(client, message.member) && message.guild.id === client.config.mainServer.id) {
 	message.delete()
