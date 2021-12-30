930f6dd59de82473cb3b287122e81be921d5e046
TÆMBØ <69138346+TAEMBO@users.noreply.github.com>
2021-12-29 21:26:18 -0800

    revamp ,staff

--- commands/staff.js
+++ commands/staff.js
@@ -1,4 +1,4 @@
-const { Client, Message, MessageEmbed } = require("discord.js");
+const { Client, Message, MessageEmbed, ClientVoiceManager } = require("discord.js");
 
 module.exports = {
     run: async (client, message, args) => {
@@ -7,18 +7,17 @@ module.exports = {
             administrator: await message.guild.roles.fetch(client.config.mainServer.roles.administrator),
             moderator: await message.guild.roles.fetch(client.config.mainServer.roles.moderator),
             trialmoderator: await message.guild.roles.fetch(client.config.mainServer.roles.trialmoderator),
+            helper: await message.guild.roles.fetch(client.config.mainServer.roles.helper)
         };
-        const admin = await staff.administrator.members.filter(x=>!x.roles.cache.has(client.config.mainServer.roles.developer)).map(e=>`<@${e.user.id}>`).join(",\n") || "None";
-        const mod = await staff.moderator.members.filter(x=>!x.roles.cache.has(client.config.mainServer.roles.administrator) && !x.roles.cache.has(client.config.mainServer.roles.developer)).map(e=>`<@${e.user.id}>`).join(",\n") || "None";
-        const tm = await staff.trialmoderator.members.map(e=>`<@${e.user.id}>`).join(",\n") || "None";
+        const admin = await staff.administrator.members.filter(x=>!x.roles.cache.has(client.config.mainServer.roles.owner)).map(e=>`<@${e.user.id}>`).join("\n") || "None";
+        const mod = await staff.moderator.members.filter(x=>!x.roles.cache.has(client.config.mainServer.roles.administrator)).map(e=>`<@${e.user.id}>`).join("\n") || "None";
+        const tm = await staff.trialmoderator.members.map(e=>`<@${e.user.id}>`).join("\n") || "None";
+        const helper = await staff.helper.members.map(e=>`<@${e.user.id}>`).join("\n") || "None";
  
         const embed = new MessageEmbed()
             .setTitle('__Staff Members__')
-            .addField(staff.administrator.name, admin)
-            .addField(staff.moderator.name, mod)
-            .addField(staff.trialmoderator.name, tm)
-            .addField("\u200b", "If you want to report someone or need any other moderation help, feel free to message anyone of these people. <@!837407028665254028> (ModMail) can also be used to report someone.")
-            .setColor(3971825)
+            .setDescription(`<@&${client.config.mainServer.roles.administrator}>\n${admin}\n\n<@&${client.config.mainServer.roles.moderator}>\n${mod}\n\n<@&${client.config.mainServer.roles.trialmoderator}>\n${tm}\n\n<@&${client.config.mainServer.roles.helper}>\n${helper}`)
+            .setColor(client.embedColor)
         message.channel.send({embeds: [embed]});
     },
     name: 'staff',
