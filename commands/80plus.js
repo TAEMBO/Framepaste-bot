4071a2fc3f05af07b449905a899b92cddcb21db6
TÆMBØ <kuneshr123@gmail.com>
2022-01-11 11:20:40 -0800

    Create 80plus.js

---
+++ commands/80plus.js
@@ -0,0 +1,9 @@
+module.exports = {
+	run: (client, message, args) => {
+		message.channel.send("https://www.guru3d.com/index.php?ct=articles&action=file&id=10061");
+	},
+	name: '80plus',
+	alias: ['80+', 'eightyplus'],
+	description: 'PSU 80+ certification levels',
+	category: 'Real Computers'
+};
