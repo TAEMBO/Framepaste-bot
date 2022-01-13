dc9a2067a4ecef68212acfd124ba9e1225a4775c
TÆMBØ <kuneshr123@gmail.com>
2022-01-12 21:18:23 -0800

    edit msg edit/delete log embeds

--- functions/logs.js
+++ functions/logs.js
@@ -4,18 +4,25 @@ module.exports = async (client) => {
    client.on("messageDelete", async (msg)=>{
        if(msg.partial) return;
        if(msg.author.bot) return;
+       const embed = new client.embed()
+           .setTitle("Message Deleted!")
+           .setDescription(`<@${msg.author.id}>\nContent:\n\`\`\`\n${msg.content}\n\`\`\`\nChannel: <#${msg.channel.id}>`)
+           .setAuthor({name: `Author: ${msg.author.tag} (${msg.author.id})`, iconURL: `${msg.author.displayAvatarURL()}`})
+           .setColor(14495300)
+           channel.send({embeds: [embed]})
        if (msg.attachments?.first()?.width && ['png', 'jpeg', 'jpg', 'gif'].some(x => msg.attachments.first().name.endsWith(x))) {
-        channel.send({embeds: [new MessageEmbed().setTitle("Message Deleted!").setDescription(`Content:\n\`\`\`\n${msg.content}\n\`\`\`\nChannel: <#${msg.channel.id}>`).setAuthor(`Author: ${msg.author.tag} (${msg.author.id})`, msg.author.displayAvatarURL({})).setColor(14495300)]})
-        channel.send({files: [msg.attachments?.first()]})
-       } else {
-        channel.send({embeds: [new MessageEmbed().setTitle("Message Deleted!").setDescription(`Content:\n\`\`\`\n${msg.content}\n\`\`\`\nChannel: <#${msg.channel.id}>`).setAuthor(`Author: ${msg.author.tag} (${msg.author.id})`, msg.author.displayAvatarURL({})).setColor(14495300)]})
-       }
-   })
+           channel.send({files: [msg.attachments?.first()]})
+   }})
    client.on("messageUpdate", async (oldMsg, newMsg)=>{
        if(oldMsg.partial) return;
        if(newMsg.partial) return;
        if(oldMsg.author.bot) return;
-       channel.send({embeds: [new MessageEmbed().setTitle("Message Edited!").setDescription(`Old Content:\n\`\`\`\n${oldMsg.content}\n\`\`\`\nNew Content:\n\`\`\`js\n${newMsg.content}\n\`\`\`\nChannel: <#${oldMsg.channel.id}>`).setAuthor(`Author: ${oldMsg.author.tag} (${oldMsg.author.id})`, oldMsg.author.displayAvatarURL({})).setColor(client.embedColor)], components: [new MessageActionRow().addComponents(new MessageButton().setStyle("LINK").setURL(`${oldMsg.url}`).setLabel("Jump to message"))]})
+       const embed = new client.embed()
+           .setTitle("Message Edited!")
+           .setDescription(`<@${oldMsg.author.id}>\nOld Content:\n\`\`\`\n${oldMsg.content}\n\`\`\`\nNew Content:\n\`\`\`js\n${newMsg.content}\n\`\`\`\nChannel: <#${oldMsg.channel.id}>`)
+           .setAuthor({name: `Author: ${oldMsg.author.tag} (${oldMsg.author.id})`, iconURL: `${oldMsg.author.displayAvatarURL()}`})
+           .setColor(client.embedColor)
+       channel.send({embeds: [embed], components: [new MessageActionRow().addComponents(new MessageButton().setStyle("LINK").setURL(`${oldMsg.url}`).setLabel("Jump to message"))]})
    })
    client.on("guildMemberAdd", async (member)=>{
         const embed = new MessageEmbed()
