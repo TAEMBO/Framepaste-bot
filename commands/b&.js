893079af77b3c9639203c129e7fbf8addecd53b5
TÆMBØ <kuneshr123@gmail.com>
2022-01-20 21:09:16 -0800

    make b& and unb& better

--- commands/b&.js
+++ commands/b&.js
@@ -1,8 +1,11 @@
 module.exports = {
 	run: (client, message, args) => {
 		message.delete().catch(err => console.log('couldnt delete message when doing b& because', err.message));
-		message.channel.send("You received an honorary ban!");
+		if (args[1]) {
+			message.channel.send(`<@${args[1]}> has received an honorary ban!`)
+		} else message.channel.send('You recieved an honorary ban!')
 	},
 	name: 'b&',
+	usage: ['userID'],
 	hidden: true
 };
--- commands/unb&.js
+++ commands/unb&.js
@@ -1,7 +1,9 @@
 module.exports = {
 	run: (client, message, args) => {
-		message.delete();
-		message.channel.send("Your honorary ban has been revoked!");
+		message.delete().catch(err => console.log('couldnt delete message when doing b& because', err.message));
+		if (args[1]) {
+			message.channel.send(`<@${args[1]}> had their honorary ban revoked!`)
+		} else message.channel.send('Your honorary ban has been revoked!')
 	},
 	name: 'unb&',
 	hidden: true
