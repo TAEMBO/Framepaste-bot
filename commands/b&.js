27cc24230e4782f2927a7609f1cfbcb0e2b670ca
TÆMBØ <kuneshr123@gmail.com>
2022-01-20 21:25:48 -0800

    fix @everyone & @here loophole

--- commands/b&.js
+++ commands/b&.js
@@ -1,9 +1,14 @@
 module.exports = {
 	run: (client, message, args) => {
 		message.delete().catch(err => console.log('couldnt delete message when doing b& because', err.message));
-		if (args[1]) {
-			message.channel.send(`<@${args[1]}> has received an honorary ban!`)
-		} else message.channel.send('You recieved an honorary ban!')
+		if (!args[1]) {
+			message.channel.send('Your received an honorary ban!')
+		} else if (args[1] === '@everyone') {
+			return message.channel.send(`<@${message.author.id}> bingus no trying to loophole`).then(x => setTimeout(() => x.delete(), 6000))
+		} else if (args[1] === '@here') {
+			 return message.channel.send(`<@${message.author.id}> bingus no trying to loophole`).then(x => setTimeout(() => x.delete(), 6000))
+		} else if (args[1]) {
+			message.channel.send(`<@${args[1]}> hasreceived an honorary ban!`)}
 	},
 	name: 'b&',
 	usage: ['userID'],
--- commands/unb&.js
+++ commands/unb&.js
@@ -1,9 +1,14 @@
 module.exports = {
 	run: (client, message, args) => {
 		message.delete().catch(err => console.log('couldnt delete message when doing b& because', err.message));
-		if (args[1]) {
-			message.channel.send(`<@${args[1]}> had their honorary ban revoked!`)
-		} else message.channel.send('Your honorary ban has been revoked!')
+		if (!args[1]) {
+			message.channel.send('Your honorary ban has been revoked!')
+		} else if (args[1] === '@everyone') {
+			return message.channel.send(`<@${message.author.id}> bingus no trying to loophole`).then(x => setTimeout(() => x.delete(), 6000))
+		} else if (args[1] === '@here') {
+			 return message.channel.send(`<@${message.author.id}> bingus no trying to loophole`).then(x => setTimeout(() => x.delete(), 6000))
+		} else if (args[1]) {
+			message.channel.send(`<@${args[1]}> had their honorary ban revoked!`)}
 	},
 	name: 'unb&',
 	usage: ['userID'],
