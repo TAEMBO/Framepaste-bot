085eea3a30b70138508dedbc21a6b388cb84896d
TÆMBØ <kuneshr123@gmail.com>
2022-01-22 18:34:19 -0800

    update response

--- commands/block.js
+++ commands/block.js
@@ -1,6 +1,6 @@
 module.exports = {
 	run: (client, message, args) => {
-		if (!client.hasModPerms(client, message.member)) return message.channel.send('You can\'t do that.');
+		if (!client.hasModPerms(client, message.member)) return message.channel.send(`You need the **${message.guild.roles.cache.get(client.config.mainServer.roles.moderator).name}** role to use this command.`);
 		if (!args[1]) return message.channel.send('You need to add a user or user ID.');
 		const userid = message.mentions.users.first()?.id || args[1];
 		client.dmForwardBlacklist.addData(userid).forceSave();
