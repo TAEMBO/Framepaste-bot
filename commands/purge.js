8b8392784ba3ce005b342563100a5799c71b35f7
TÆMBØ <kuneshr123@gmail.com>
2022-01-22 18:45:51 -0800

    delete correct amount of messages

--- commands/purge.js
+++ commands/purge.js
@@ -5,7 +5,7 @@ module.exports = {
 		//if (!client.hasModPerms(client, message.member)) return message.channel.send(`You need the **${message.guild.roles.cache.get(client.config.mainServer.roles.moderator).name}** role to use this command.`);
 		if (!amount) return message.channel.send('You need to specify an amount of messages to delete.');
 		if (amount > 100) return message.channel.send('You can only delete 100 messages at once. This is a Discord API limitation.');
-		const deleted = await message.channel.bulkDelete(amount).catch(err => message.channel.send('Something went wrong while deleting messages.'));
+		const deleted = await message.channel.bulkDelete(amount + 1).catch(err => message.channel.send('Something went wrong while deleting messages.'));
 		message.channel.send(`Deleted **${deleted.size}** messages.`).then(x => setTimeout(() => x.delete(), 4000));
 	},
 	name: 'purge',
