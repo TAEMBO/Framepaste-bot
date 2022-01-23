beab1190c7056d338be5790217aa4db541e54643
TÆMBØ <kuneshr123@gmail.com>
2022-01-22 21:08:16 -0800

    Update removebannedword.js

--- commands/removebannedword.js
+++ commands/removebannedword.js
@@ -3,6 +3,7 @@ module.exports = {
 		if (!client.hasModPerms(client, message.member)) return message.channel.send(`You need the **${message.guild.roles.cache.get(client.config.mainServer.roles.moderator).name}** role to use this command.`);
 		if (!args[1]) return message.channel.send('You need to include the word to unban.');
 		delete client.bannedWords._content[args[1]]
+        client.bannedWords.forceSave();
 		message.channel.send('Successfully unbanned the word.');
 	},
 	name: 'removebannedword',
