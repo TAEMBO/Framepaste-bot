1c8d03066b80b6d30aa08e5109f5bd5d10ca9081
TÆMBØ <kuneshr123@gmail.com>
2022-01-22 18:39:59 -0800

    Revert "Update block.js"
    This reverts commit 5626e54b37e51380bf5c377f1598806ccbeb25c2.

--- commands/block.js
+++ commands/block.js
@@ -7,7 +7,7 @@ module.exports = {
 		message.channel.send('Successfully blocked user ' + userid);
 	},
 	name: 'block',
-	usage: ['word'],
+	usage: ['user id / mention'],
 	description: 'Block user from sending DMs to the bot or ModMail. Used as a punishment for users who abuse the aforementioned features.',
 	shortDescription: 'Block user from DMing bot.',
 	category: 'Moderation'
