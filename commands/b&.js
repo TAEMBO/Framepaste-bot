5afff2514beef59a9f5e6c5ecf7e4f1d74659013
TÆMBØ <kuneshr123@gmail.com>
2022-01-24 00:12:05 -0800

    grammar fixes

--- commands/b&.js
+++ commands/b&.js
@@ -2,7 +2,7 @@ module.exports = {
 	run: (client, message, args) => {
 		message.delete().catch(err => console.log('couldnt delete message when doing b& because', err.message));
 		if (!args[1]) {
-			message.channel.send('Your received an honorary ban!')
+			message.channel.send('You received an honorary ban!')
 		} else if (args[1] === '@everyone') {
 			return message.channel.send(`<@${message.author.id}> bingus no trying to loophole`).then(x => setTimeout(() => x.delete(), 6000))
 		} else if (args[1] === '@here') {
@@ -12,7 +12,7 @@ module.exports = {
 	    } else if (args[1] === 'here') {
 			 return message.channel.send(`<@${message.author.id}> bingus no trying to loophole`).then(x => setTimeout(() => x.delete(), 6000))
 		} else if (args[1]) {
-			message.channel.send(`<@${args[1]}> hasreceived an honorary ban!`)}
+			message.channel.send(`<@${args[1]}> has received an honorary ban!`)}
 	},
 	name: 'b&',
 	usage: ['userID'],
