f6f573100b239831a52d5ce58fb7f55b400aa97a
TÆMBØ <kuneshr123@gmail.com>
2022-01-24 10:10:10 -0800

    smarter word collector by Bostengard

--- commands/addbannedword.js
+++ commands/addbannedword.js
@@ -2,7 +2,7 @@ module.exports = {
 	run: (client, message, args) => {
 		if (!client.hasModPerms(client, message.member)) return message.channel.send(`You need the **${message.guild.roles.cache.get(client.config.mainServer.roles.moderator).name}** role to use this command.`);
 		if (!args[1]) return message.channel.send('You need to add a word to ban.');
-		client.bannedWords.addData(args[1]).forceSave();
+		client.bannedWords.addData(args.slice(1).join(' ')).forceSave();
 		message.channel.send('Successfully added banned word.');
 	},
 	name: 'addbannedword',
