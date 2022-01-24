01783994e14e8dc8754132c15bc0322fd2e0e9e0
TÆMBØ <kuneshr123@gmail.com>
2022-01-24 10:15:12 -0800

    Update addbannedword.js

--- commands/addbannedword.js
+++ commands/addbannedword.js
@@ -3,11 +3,11 @@ module.exports = {
 		if (!client.hasModPerms(client, message.member)) return message.channel.send(`You need the **${message.guild.roles.cache.get(client.config.mainServer.roles.moderator).name}** role to use this command.`);
 		if (!args[1]) return message.channel.send('You need to add a word to ban.');
 		client.bannedWords.addData(args.slice(1).join(' ')).forceSave();
-		message.channel.send('Successfully added banned word.');
+		message.channel.send(`Successfully added \`${args.slice(1).join(' ')}\` to bannedWords list`);
 	},
 	name: 'addbannedword',
     alias: ['abw', 'bannedword'],
-	usage: ['word', 'phrase'],
+	usage: ['word/phrase'],
 	description: 'Add a word to the bannedWords database',
 	category: 'Moderation'
 };
