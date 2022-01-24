da0d29f629f3b7dbff3f14d692e02c25bd03f5f5
TÆMBØ <kuneshr123@gmail.com>
2022-01-23 23:15:05 -0800

    banned words: edits ignore other servers

--- index.js
+++ index.js
@@ -816,7 +816,7 @@ client.on("messageCreate", async (message) => {
 });
 // handle banned words: edits
 client.on("messageUpdate", async (oldMsg, newMsg)=>{
-	if (client.bannedWords._content.some(word => newMsg.content.toLowerCase().includes(word))) {
+	if (client.bannedWords._content.some(word => newMsg.content.toLowerCase().includes(word)) && newMsg.guild.id === client.config.mainServer.id) {
 		newMsg.delete();
 }})
 
