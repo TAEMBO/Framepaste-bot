9b35333db37b0ce29355d8ba562402535eb80dad
TÆMBØ <kuneshr123@gmail.com>
2022-01-24 22:24:06 -0800

    Create iwillsendutojesus.js

---
+++ commands/iwillsendutojesus.js
@@ -0,0 +1,10 @@
+module.exports = {
+	run: (client, message, args) => {
+		message.delete();
+		message.channel.send("https://c.tenor.com/Z3yhizAhKRsAAAAC/steven-he-i-will.gif");
+	},
+	name: 'iwillsendutojesus',
+	alias: ['iwillsendu2jesus','sendtojesus'],
+	description: `Made by ItzDihan7674 (for real again)`,
+	hidden: true
+};
