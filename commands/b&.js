3451863103133af8c2f5d50c984bad8d8c66c4a0
Stif <79871802+stifskere@users.noreply.github.com>
2022-02-08 19:44:36 +0100

    Try that now
    It must work for both id and mention

--- commands/b&.js
+++ commands/b&.js
@@ -1,18 +1,15 @@
 module.exports = {
 	run: (client, message, args) => {
 		message.delete().catch(err => console.log('couldnt delete message when doing b& because', err.message));
+		
+		let member = args(1).toString().replace(/[\\<>@#&!']/g, "")
+		
 		if (!args[1]) {
 			message.channel.send('You received an honorary ban!')
-		} else if (args[1] === '@everyone') {
+		} else if (args[1] === '@everyone' || '@here' || 'everyone' || 'here') {
 			return message.channel.send(`<@${message.author.id}> bingus no trying to loophole`).then(x => setTimeout(() => x.delete(), 6000))
-		} else if (args[1] === '@here') {
-			 return message.channel.send(`<@${message.author.id}> bingus no trying to loophole`).then(x => setTimeout(() => x.delete(), 6000))
-        } else if (args[1] === 'everyone') {
-			return message.channel.send(`<@${message.author.id}> bingus no trying to loophole`).then(x => setTimeout(() => x.delete(), 6000))
-	    } else if (args[1] === 'here') {
-			 return message.channel.send(`<@${message.author.id}> bingus no trying to loophole`).then(x => setTimeout(() => x.delete(), 6000))
 		} else if (args[1]) {
-			message.channel.send(`<@${args[1]}> has received an honorary ban!`)}
+			message.channel.send(`<@${member}> has received an honorary ban!`)}
 	},
 	name: 'b&',
 	usage: ['userID'],
