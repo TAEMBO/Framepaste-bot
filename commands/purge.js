b9bab15416376b27302caec5d90f40eed0be4f16
TÆMBØ <kuneshr123@gmail.com>
2022-02-07 21:02:16 -0800

    Update purge.js

--- commands/purge.js
+++ commands/purge.js
@@ -1,8 +1,8 @@
 module.exports = {
 	run: async (client, message, args) => {
-		if (message.guild.id !== client.config.mainServer.id) return message.reply('wrong server');
+		if (message.guild.id !== client.config.mainServer.id) return message.reply('Wrong server.');
 		const amount = parseInt(args[1]);
-		//if (!client.hasModPerms(client, message.member)) return message.reply(`You need the **${message.guild.roles.cache.get(client.config.mainServer.roles.moderator).name}** role to use this command.`);
+		if (!client.hasModPerms(client, message.member)) return message.reply(`You need the **${message.guild.roles.cache.get(client.config.mainServer.roles.moderator).name}** role to use this command.`);
 		if (!amount) return message.reply({content: 'You need to specify an amount of messages to delete.', allowedMentions: { repliedUser: false }});
 		if (amount > 100) return message.reply({content: 'You can only delete 100 messages at once. This is a Discord API limitation.', allowedMentions: { repliedUser: false }});
 		const deleted = await message.channel.bulkDelete(amount + 1).catch(err => message.channel.send('Something went wrong while deleting messages.'));
