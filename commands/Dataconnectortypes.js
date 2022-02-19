6ea607be4c2237c88e2105d05ca89d305eb1e27b
Stif <stifskere@gmail.com>
2022-02-19 01:42:36 +0100

    bing chilling
    added Dataconnectortypes command or dc command and the image folder, all images must be replaced locally to that since yassir said cdn links can expire

---
+++ commands/Dataconnectortypes.js
@@ -0,0 +1,15 @@
+const discord = require("discord.js")
+const path = require("path")
+module.exports = {
+    run: async (client, message) => {
+        const { MessageEmbed, MessageAttachment } = require("discord.js");
+        const image = await new MessageAttachment("./media/Connectortypes.png", "dataimage.png");
+        const dataConnectorsEmbed = new MessageEmbed().setTitle("Data Connector Types").setImage(`attachment://dataimage.png`);
+        message.reply({embeds: [dataConnectorsEmbed], files: [image]})
+    },
+    name: 'dataconnectors',
+    description: 'Data connector types for hard drives',
+    category: 'Real Computers',
+    alias: ['dc', 'driveconnectors']
+
+};
