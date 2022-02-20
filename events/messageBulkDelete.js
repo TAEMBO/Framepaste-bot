d1bb0e8a2dc94dcff80abba81bbeb020be982e48
Yasser-Alshinawah <yasserrose@icloud.com>
2022-02-20 01:31:23 +0000

    merge

--- events/messageBulkDelete.js
+++ events/messageBulkDelete.js
@@ -3,7 +3,7 @@ module.exports = {
     name: "messageBulkDelete",
     giveaway: false,
     tracker: false,
-    execute: async () => {
+    execute: async (client, messages) => {
         const channel = await client.channels.fetch(require("../config.json").mainServer.channels.modlogs);
          if (!client.config.botSwitches.automod) return;
          let text = "";
@@ -11,7 +11,7 @@ module.exports = {
              text += `${e.author.tag}: ${e.content}\n`;
          });
          const embed = new MessageEmbed()
-         .setDescription(`\`\`\`${text}\`\`\``)
+         .setDescription(`\`\`\`${text}\`\`\``.slice(0, 4100))
          .setTitle(`${messages.size} Messages Were Deleted.`)
          .addField("Channel", `<#${messages.first().channel.id}>`)
          .setColor(client.embedColor)
