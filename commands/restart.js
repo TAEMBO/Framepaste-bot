1e6ae4daacd8d90a36340ba28d0009fdcbc15334
TÆMBØ <kuneshr123@gmail.com>
2022-02-22 13:13:36 -0800

    Update restart.js

--- commands/restart.js
+++ commands/restart.js
@@ -3,7 +3,7 @@ module.exports = {
     if (!client.hasModPerms(client, message.member) || !message.member.roles.cache.has(client.config.mainServer.roles.botdeveloper)) return message.reply({content: `You need the **${message.guild.roles.cache.get(client.config.mainServer.roles.moderator).name}** role to use this command.`, allowedMentions: {repliedUser: false}})
     message.reply("Restarting...").then(async ()=> eval(process.exit(-1)))
   },
-    name: 'rest',
+    name: 'restart',
     description: 'Restarts the bots',
     category: 'Moderation',
     hidden: true
