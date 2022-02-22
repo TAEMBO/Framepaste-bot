33332c0b13ff156ee54e33cdfc04303d48a326c9
Stif <stifskere@gmail.com>
2022-02-22 14:12:03 +0100

    WORKING RESTART COMMAND

--- commands/restart.js
+++ commands/restart.js
@@ -1,4 +1,4 @@
-
+const { exec } = require("child_process");
 module.exports = {
   run: (client, message) => {
     if(!message.member.roles.cache.has("858077018732888084" || "914646059714768906")){
@@ -6,20 +6,9 @@ module.exports = {
       return;
     }
 
-    function timeFunction() {
-      message.channel.send("bot restarting.")
-      setTimeout(function(){resetBot(message.channel);}, 5000);
-    }
-
-    timeFunction();
-
-    async function resetBot(channel) {
-      channel.send('Bot restarted.')
-          .then(msg => client.destroy())
-          .then(() => client.login(client.config.token), client.login(client.config.modmailBotToken));
-      await client.channels.fetch(require("../config.json").mainServer.channels.modlogs).then((channel)=>{channel.send(`:warning: Bot restarted :warning:\n${client.config.eval.whitelist.map(x => `<@${x}>`).join(' ')}`)});
-            }
+    message.channel.send("Bot is restarting").then(async ()=> exec("pm2 restart 0"))
 
   },
-    name: 'restart'
+    name: 'restart',
+    category: 'Moderation'
 };
