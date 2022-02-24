2958da1f2ee63d400f41aa0807c7a44cd694be23
TÆMBØ <kuneshr123@gmail.com>
2022-02-23 21:22:07 -0800

    Delete pull.js

--- commands/pull.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    run: (client, message, args) => {
-        exec("git commit -m \"merge\""); exec("git pull")
-        message.reply({content: 'Pulling available updates...', allowedMentions: {repliedUser: false}})
-    },
-    name: 'pull',
-    description: 'Pull GitHub updates to bot',
-    hidden: true
-};
