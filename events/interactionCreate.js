1bbf68f5e8c46a2fb593da9bfdb9e696cb8a3665
TÆMBØ <kuneshr123@gmail.com>
2022-03-05 22:22:26 -0800

    fix: part 2

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,4 +1,4 @@
-const { MessageEmbed } = require("discord.js");
+const { MessageEmbed, MessageActionRow, MessageButton } = require("discord.js");
 
 module.exports = {
     name: "interactionCreate",
