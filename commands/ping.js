6ed7c3caeebc2df41a1ea89ddbed871207782076
TÆMBØ <kuneshr123@gmail.com>
2022-03-15 12:10:13 -0700

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,9 +1,9 @@
 module.exports = { 
-	run: (client, message, args) => {
-		message.reply({content: `WebSocket Latency: **${client.ws.ping}ms**.`, allowedMentions: { repliedUser: false }});
-	},
-	name: 'ping',
-	description: 'Average latency of bot client\'s shards\' last websocket heartbeat from sending to ack in milliseconds. No it\'s not _your_ ping.',
-	shortDescription: 'Average Websocket latency',
-	category: 'Bot'
+    run: async (client, message, args) => {
+        const msg = await message.reply({content: "Ping:", allowedMentions: {repliedUser: false}});
+        const time = msg.createdTimestamp - message.createdTimestamp; msg.edit({content: `Ping: \`${time}\`ms`});
+    },
+    name: 'ping',
+    description: 'Bot\'s latency',
+    category: 'Bot'
 };
