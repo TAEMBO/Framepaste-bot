48214a8b38b6ea0d55ba67a20c1d086758211406
TÆMBØ <kuneshr123@gmail.com>
2022-03-15 18:04:22 -0700

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,7 +1,7 @@
 module.exports = { 
     run: async (client, message, args) => {
         const msg = await message.reply({content: "Ping:", allowedMentions: {repliedUser: false}});
-        const time = msg.createdTimestamp - message.createdTimestamp; msg.edit({content: `Ping: \`${time}\`ms`});
+        const time = msg.createdTimestamp - message.createdTimestamp; msg.edit({content: `Ping: \`${time}\`ms`, allowedMentions: {repliedUser: false}});
     },
     name: 'ping',
     description: 'Bot\'s latency',
