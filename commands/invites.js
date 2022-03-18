26d4823dfa95703d915020f20f2edc4a39049d3d
Yasser-Alshinawah <yasserrose@icloud.com>
2022-03-18 16:58:23 +0000

    bye bye error

--- commands/invites.js
+++ commands/invites.js
@@ -1,10 +1,11 @@
 const {SlashCommandBuilder} = require("@discordjs/builders");
 module.exports = {
 	run: async (client, interaction) => {
+		// /if(interaction.guild.id !== client.config.mainServer.id) return interaction.reply({content: "This command dosen't work here", ephemeral: true});
 		const user = interaction.options.getUser("member") ?? interaction.user;
 		const invitz = await interaction.guild.invites.fetch();
 		const invitez = invitz.filter(x=>x.inviterId===user.id).map(x=>x.uses);
-		const invites = invitez.reduce((partialSum, a) => partialSum + a);
+		const invites = invitez.length !== 0 ? invitez?.reduce((partialSum, a) => partialSum + a) : 0;
 		const embed = new client.embed().setTitle("Invites").setDescription(`${user.id === interaction.user.id ? "You have" : `<@${user.id}> has`} ${invites} invites.`).setColor(client.config.embedColor);
 		interaction.reply({embeds: [embed]})
 	},
