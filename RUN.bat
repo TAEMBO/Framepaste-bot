9a6715135ac45990216ee7f067fbcaeae8e255f4
TÆMBØ <kuneshr123@gmail.com>
2022-03-20 20:29:12 -0700

    remove backup

--- RUN.bat
+++ RUN.bat
@@ -2,5 +2,4 @@
 cls
 color 4
 node sharding.js
-node databases/backup.js
 pause
--- databases/backup.js
+++
@@ -1,8 +0,0 @@
-const { exec } = require("child_process");
-console.log("Database backup loaded")
-setInterval(async () => {
-await exec("git add .");
-await exec("git commit -m \"Scheduled commits\"");
-await exec("git push");
-console.log("Database backed up")
-}, 10*60*60*2)
