e3e8932b5cd71b354683a85d06bb99fc443885c8
TÆMBØ <kuneshr123@gmail.com>
2022-03-23 21:28:07 -0700

    fix

--- commands/update.js
+++ commands/update.js
@@ -1,7 +1,7 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
 
 module.exports = {
-    run: (client, interaction) => {
+    run: async (client, interaction) => {
         if (!client.hasModPerms(client, interaction.member) && !interaction.member.roles.cache.has(client.config.mainServer.roles.botdeveloper)) return interaction.reply({content: `You need the **${interaction.guild.roles.cache.get(client.config.mainServer.roles.moderator).name}** role to use this command.`, allowedMentions: {repliedUser: false}})
         const msg = await interaction.reply({content: "Pulling from repo..."});
         require("child_process").exec("git pull").then(async ()=> msg.edit({content: "Pulled from repo"}));
