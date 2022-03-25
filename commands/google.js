ac7f4b1dcfd5282f5706ec6fdc348e0a7a87f24a
Yasser <73202985+Yasser-Alshinawah@users.noreply.github.com>
2022-03-25 17:26:29 -0400

    Update google.js

--- commands/google.js
+++ commands/google.js
@@ -2,10 +2,7 @@ const { SlashCommandBuilder } = require("@discordjs/builders");
 
 module.exports = {
 	run: (client, interaction) => {
-		const content = `https://www.google.com/search?q=${new URLSearchParams(interaction.options.getString("query")).split(" ").join("+")}`
-		if(content.substr(-1) === "="){
-		content = content.slice(0, content.lenght-1)
-		}
+		const content = `https://www.google.com/search?q=${interaction.options.getString("query").split(" ").join("+")}`
         interaction.reply({content})
 	},
 	data: new SlashCommandBuilder().setName("google").setDescription("Googles something.").addStringOption((opt)=>opt.setName("query").setDescription("What to search for.").setRequired(true))
