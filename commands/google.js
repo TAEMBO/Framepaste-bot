e59f7e202a93b7bc6f9e14e32a15fd319b792786
Stif <stifskere@gmail.com>
2022-03-25 22:13:13 +0100

    Google

--- commands/google.js
+++ commands/google.js
@@ -2,7 +2,9 @@ const { SlashCommandBuilder } = require("@discordjs/builders");
 
 module.exports = {
 	run: (client, interaction) => {
-        interaction.reply(`https://www.google.com/search?q=${new URLSearchParams(interaction.options.getString("query")).toString()}`)
+		let content = `https://www.google.com/search?q=${new URLSearchParams(interaction.options.getString("query")).toString()}`
+		content = content.replaceAll("F=", "F")
+        interaction.reply(content)
 	},
 	data: new SlashCommandBuilder().setName("google").setDescription("Googles something.").addStringOption((opt)=>opt.setName("query").setDescription("What to search for.").setRequired(true))
 };
