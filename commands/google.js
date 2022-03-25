0285ef4b70391e340578405b2db75fc0743f9558
TÆMBØ <kuneshr123@gmail.com>
2022-03-25 14:24:54 -0700

    Revert "Update google.js"
    This reverts commit d042873d29ff7fc10b0f010ff6740d07c09438f4.

--- commands/google.js
+++ commands/google.js
@@ -2,7 +2,7 @@ const { SlashCommandBuilder } = require("@discordjs/builders");
 
 module.exports = {
 	run: (client, interaction) => {
-		const content = `https://www.google.com/search?q=${interaction.options.getString("query")).split(" ").join("+")}`
+		const content = `https://www.google.com/search?q=${new URLSearchParams(interaction.options.getString("query")).split(" ").join("+")}`
         interaction.reply({content})
 	},
 	data: new SlashCommandBuilder().setName("google").setDescription("Googles something.").addStringOption((opt)=>opt.setName("query").setDescription("What to search for.").setRequired(true))
