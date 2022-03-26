9d11d46bc7764fcadf74f231d7347fe656873d8b
TÆMBØ <kuneshr123@gmail.com>
2022-03-25 19:26:29 -0700

    add tag for server owner

--- commands/whois.js
+++ commands/whois.js
@@ -14,6 +14,7 @@ module.exports = {
 			.addField(':small_blue_diamond: Roles', member.roles.cache.size > 1 ? member.roles.cache.filter(x => x.id !== interaction.guild.roles.everyone.id).sort((a, b) => b.position - a.position).map(x => x).join(member.roles.cache.size > 4 ? ' ' : '\n').slice(0, 1024) : 'None')
 			.setColor(member.displayColor || client.config.embedColor)
 			.setImage(member.user.bannerURL({ format: 'png', dynamic: true, size: 1024}))
+			if (member.user.id === interaction.guild.ownerId) embed.setDescription('__**Server Owner <:crown:945149846506078250>')
 		interaction.reply({embeds: [embed], allowedMentions: { repliedUser: false }});
 	},
 	data: new SlashCommandBuilder().setName("whois").setDescription("Gets info on a user.").addUserOption((opt)=>opt.setName("member").setDescription("The member to get info on.").setRequired(true))
