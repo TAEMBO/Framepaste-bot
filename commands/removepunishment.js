ee3dbbe4ea2be8d8ef8836fb3f5a4f73d84fb0f6
Yasser-Alshinawah <yasserrose@icloud.com>
2022-03-27 03:43:43 +0000

    false => true

--- commands/removepunishment.js
+++ commands/removepunishment.js
@@ -4,5 +4,5 @@ module.exports = {
 	run: (client, interaction) => {
 		client.unPunish(client, interaction, 'ban');
 	},
-	data: new SlashCommandBuilder().setName("unpunish").setDescription("Unpunishes a user.").addIntegerOption((opt)=>opt.setName("case_id").setDescription("The ID of the punishment to remove.").setRequired(false)).addStringOption((opt)=>opt.setName("reason").setDescription("The reason for removing the punishment.").setRequired(false))
+	data: new SlashCommandBuilder().setName("unpunish").setDescription("Unpunishes a user.").addIntegerOption((opt)=>opt.setName("case_id").setDescription("The ID of the punishment to remove.").setRequired(true)).addStringOption((opt)=>opt.setName("reason").setDescription("The reason for removing the punishment.").setRequired(false))
 };
