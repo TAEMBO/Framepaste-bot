d72106dd6bad1d0be4131e8785f2e6edba6b677a
TÆMBØ <kuneshr123@gmail.com>
2022-03-28 18:05:49 -0700

    update lrs perks

--- commands/levelroles.js
+++ commands/levelroles.js
@@ -157,7 +157,7 @@ module.exports = {
 
 			const embed = new client.embed()
 				.setTitle('Level Roles: Perks')
-				.setDescription(`<@&${client.config.mainServer.roles.levels.three.id}> - Use External Sticker permissions\n<@&${client.config.mainServer.roles.levels.five.id}> - Permission to create public & private threads\n<@&${client.config.mainServer.roles.levels.seven.id}> - Use bot commands anywhere\n<@&${client.config.mainServer.roles.levels.ten.id}> - Hoisted\n<@&${client.config.mainServer.roles.levels.eleven.id}> - Hoisted\n<@&${client.config.mainServer.roles.levels.twelve.id}> - Hoisted\n<@&${client.config.mainServer.roles.levels.thirteen.id}> - Hoisted\n<@&${client.config.mainServer.roles.levels.fourteen.id}> - Hoisted\n<@&${client.config.mainServer.roles.levels.fifteen.id}> - Hoisted`)
+				.setDescription(`<@&${client.config.mainServer.roles.levels.three.id}> - Use External Sticker permissions\n<@&${client.config.mainServer.roles.levels.five.id}> - Permission to create public & private threads\n<@&${client.config.mainServer.roles.levels.seven.id}> - Use bot commands anywhere\n<@&${client.config.mainServer.roles.levels.nine.id}> - See all channels in <#931762419171201024>\n<@&${client.config.mainServer.roles.levels.ten.id}> - Hoisted\n<@&${client.config.mainServer.roles.levels.eleven.id}> - Hoisted\n<@&${client.config.mainServer.roles.levels.twelve.id}> - Hoisted\n<@&${client.config.mainServer.roles.levels.thirteen.id}> - Hoisted\n<@&${client.config.mainServer.roles.levels.fourteen.id}> - Hoisted\n<@&${client.config.mainServer.roles.levels.fifteen.id}> - Hoisted`)
 				.setColor(client.config.embedColor)
 			interaction.reply({embeds: [embed], allowedMentions: { repliedUser: false }});
 			return;
