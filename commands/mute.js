afe254cec239b5c02bed5ff29edb7a7a7ad6f134
Stif <stifskere@gmail.com>
2022-04-02 06:57:38 +0200

    mute

--- commands/mute.js
+++ commands/mute.js
@@ -1,6 +1,14 @@
 const {SlashCommandBuilder} = require("@discordjs/builders")
 module.exports = {
 	run: (client, interaction) => {
+
+		const userToPunish = interaction.options.getUser('member')
+
+		if(interaction.user === userToPunish){
+			interaction.reply({content: "You can't mute yourself", ephemeral: true})
+			return;
+		}
+
 		client.punish(client, interaction, 'mute');
 	},
 	data: new SlashCommandBuilder().setName("mute").setDescription("Mutes a member.").addUserOption((opt)=>opt.setName("member").setDescription("The member to mute").setRequired(true)).addStringOption((opt)=>opt.setName("time").setDescription("The time for the mute.")).addStringOption((opt)=>opt.setName("reason").setDescription("The reason for banning the member.").setRequired(false)),
