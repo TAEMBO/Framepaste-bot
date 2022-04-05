9bcacd0335d5d693c459e938c4aef7aa599f67bd
TÆMBØ <kuneshr123@gmail.com>
2022-04-05 12:03:42 -0700

    fix desc replacement with owner text

--- commands/whois.js
+++ commands/whois.js
@@ -16,7 +16,7 @@ module.exports = {
 			{name: '🔹 Roles', value: member.roles.cache.size > 1 ? member.roles.cache.filter(x => x.id !== interaction.guild.roles.everyone.id).sort((a, b) => b.position - a.position).map(x => x).join(member.roles.cache.size > 4 ? ' ' : '\n').slice(0, 1024) : 'None'})
 			.setColor(member.displayColor || client.config.embedColor)
 			.setImage(member.user.bannerURL({ format: 'png', dynamic: true, size: 1024}))
-			if (member.user.id === interaction.guild.ownerId) embed.setDescription('__**Server Owner**__ <:crown:945149846506078250>')
+			if (member.user.id === interaction.guild.ownerId) embed.setDescription(`<@${member.user.id}>\n\`${member.user.id}\`\n__**Server Owner**__ <:crown:945149846506078250>`)
 		interaction.reply({embeds: [embed], allowedMentions: { repliedUser: false }});
 	},
 	data: new SlashCommandBuilder().setName("whois").setDescription("Gets info on a user.").addUserOption((opt)=>opt.setName("member").setDescription("The member to get info on.").setRequired(true))
