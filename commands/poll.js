b8a86fbc4cc2b1f229a7be61f166a9027877bf77
Yasser-Alshinawah <yasserrose@icloud.com>
2022-04-21 20:35:41 +0000

    ok

--- commands/poll.js
+++ commands/poll.js
@@ -9,7 +9,7 @@ module.exports = {
             .setName('content')
             .setDescription('The poll content')
             .setRequired(true)),
-    run: (client, interaction) => {
+    run: async (client, interaction) => {
 
         const content = interaction.options.getString("content");
 
@@ -18,18 +18,20 @@ module.exports = {
             ephemeral: true
         }) 
         
-        const inte = await interaction.reply({fetchReply: true, embeds: [new client.embed().setAuthor({name: "This poll expires in 15 minutes."}).setTitle("New Poll.").setColor(client.config.embedColor).setDescription("**my poll name**")], components: [new Discord.MessageActionRow().addComponents(new Discord.MessageButton().setStyle("SUCCESS").setLabel("1").setCustomId(`upvate`).setEmoji("<:upvote:708964481413283891>"), new Discord.MessageButton().setStyle("DANGER").setLabel("1").setCustomId("downbad").setEmoji("<:downvote:910416524840423435> "))]})
+        const inte = await interaction.reply({fetchReply: true, embeds: [new client.embed().setAuthor({name: "This poll expires in 15 minutes."}).setTitle("New Poll.").setColor(client.config.embedColor).setDescription(`**${content}**`)], components: [new Discord.MessageActionRow().addComponents(new Discord.MessageButton().setStyle("SUCCESS").setLabel("1").setCustomId(`upvate`).setEmoji("<:upvote:708964481413283891>"), new Discord.MessageButton().setStyle("DANGER").setLabel("1").setCustomId("downbad").setEmoji("<:downvote:910416524840423435> "))]})
 
         const collecctor = inte.createMessageComponentCollector({time: 900000});
         const votes = [];
-        collector.on("collect", async (i) => {
+        collecctor.on("collect", async (i) => {
             if (votes.includes(i.user.id)) {
                 return i.reply({content: 'You already voted', ephemeral: true});
             } else if (i.customId === 'upvate') {
-                interaction.update({embeds: [i.message.embeds[0]], components: [new Discord.MessageActionRow().addComponents(i.message.components[0].components[0].setLabel(`${parseInt(i.message.components[0].components[0].label) + 1}`))]});
+                i.update({embeds: [i.message.embeds[0]], components: [new Discord.MessageActionRow().addComponents(i.message.components[0].components[0].setLabel(`${parseInt(i.message.components[0].components[0].label) + 1}`), i.message.components[0].components[1])]});
+                votes.push(i.user.id);
             } else if (i.customId === 'votedown') {
-                interaction.update({embeds: [i.message.embeds[0]], components: [new Discord.MessageActionRow().addComponents(i.message.components[0].components[1].setLabel(`${parseInt(i.message.components[0].components[1].label) + 1}`))]});
-        }
+                i.update({embeds: [i.message.embeds[0]], components: [new Discord.MessageActionRow().addComponents(i.message.components[0].components[0], i.message.components[0].components[1].setLabel(`${parseInt(i.message.components[0].components[1].label) + 1}`))]});
+                votes.push(i.user.id);
+            }
         collecctor.on("end", async (ia) => {
             inte.edit({embeds: [inte.embeds[0]], components: [new Discord.MessageActionRow().addComponents(inte.components[0].components[0].forEach((e)=>{e.setDisabled(true)}))]})
         });
