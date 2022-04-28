f64a598548a3a8e7ce7b1db41bf651235690e480
TÆMBØ <kuneshr123@gmail.com>
2022-04-27 17:21:37 -0700

    fix count

--- commands/purge.js
+++ commands/purge.js
@@ -5,8 +5,8 @@ module.exports = {
 		const amount = interaction.options.getInteger("amount");
 		if (!client.hasModPerms(client, interaction.member)) return interaction.reply({content: `You need the <@&${interaction.guild.roles.cache.get(client.config.mainServer.roles.mod).id}> role to use this command.`, allowedMentions: {roles: false}});
 		if (amount > 100) return interaction.reply({content: 'You can only delete 100 messages at once. This is a Discord API limitation.', allowedMentions: { repliedUser: false }});
-		const deleted = await interaction.channel.bulkDelete(amount + 1).catch(err => interaction.reply('Something went wrong while deleting messages.' + err.interaction));
-		interaction.reply(`Deleted **${deleted.size - 1}** messages.`).then(x => setTimeout(() => interaction.deleteReply(), 4000));
+		const deleted = await interaction.channel.bulkDelete(amount).catch(err => interaction.reply('Something went wrong while deleting messages.' + err.interaction));
+		interaction.reply(`Deleted **${deleted.size}** messages.`).then(x => setTimeout(() => interaction.deleteReply(), 4000));
 	},
 	data: new SlashCommandBuilder().setName("purge").setDescription("Purges messages in a channel.").addIntegerOption((opt)=>opt.setName("amount").setDescription("The amount of messages to purge.").setRequired(true))
 };
