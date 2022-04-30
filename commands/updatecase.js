a527b311f505dd2b1beedf301529062f5fe7dd7f
Stif <stifskere@gmail.com>
2022-04-30 16:38:30 +0200

    A

---
+++ commands/updatecase.js
@@ -0,0 +1,35 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { MessageEmbed } = require("discord.js");
+const casesJson = require("../databases/punishments.json");
+const fs = require("node:fs");
+const path = require("node:path");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('update')
+        .setDescription('Update a case')
+        .addSubcommand(a => a
+            .setName('case')
+            .setDescription('Update a case')
+            .addIntegerOption(caseid => caseid
+                .setName('caseid')
+                .setDescription('The case ID')
+                .setRequired(true))
+            .addStringOption(descriiption => descriiption
+                .setName('reason')
+                .setDescription('The new case reason')
+                .setRequired(true))),
+    run: async (client, interaction) => {
+
+        const caseid = interaction.options.getInteger('caseid');
+        const reason = interaction.options.getString('reason');
+
+        casesJson.forEach(casee => {
+            if (casee.id === caseid) {
+                casee.reason = reason;
+            }
+        });
+
+        fs.writeFileSync(path.resolve('../databases/punishments.json'), JSON.stringify(casesJson));
+    }
+}
