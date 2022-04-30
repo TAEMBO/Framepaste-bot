1030930983235a3c8b375e98782d97ace4b713c5
Stif <stifskere@gmail.com>
2022-04-30 16:52:07 +0200

    bro fr

--- commands/updatecase.js
+++ commands/updatecase.js
@@ -27,6 +27,6 @@ module.exports = {
             }
         });
 
-        fs.writeFileSync(path.resolve('databases/punishments.json'), JSON.stringify(casesJson));
+        fs.writeFileSync(`${__dirname}/databases/punishments.json`, JSON.stringify(casesJson));
     }
 }
