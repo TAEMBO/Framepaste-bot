4dae701dca08e65d0fa8eb277799de205ea7c573
Stif <stifskere@gmail.com>
2022-05-03 21:41:09 +0200

    stupid thing

--- client.js
+++ client.js
@@ -376,17 +376,18 @@ class YClient extends Client {
     };
     async unPunish(client, interaction) {
         if (!client.hasModPerms(client, interaction.member)) return interaction.reply({content: `You need the <@&${interaction.guild.roles.cache.get(client.config.mainServer.roles.mod).id}> role to use this command.`, ephemeral: true, allowedMentions: {roles: false}});
-        const punishment = client.punishments._content.find(x => x.id == `${interaction.options.getInteger("case_id")}`);
+        const punishment = client.punishments._content.find(x => x.id === `${interaction.options.getInteger("case_id")}`);
         if (!punishment) return interaction.reply({content: "that isn't a valid case ID.", ephemeral: true});
         if (punishment.type !== 'warn' && interaction.member.roles.cache.has(client.config.mainServer.roles.minimod)) return interaction.reply({content: 'Trial moderators can only remove warnings.', ephemeral: true, allowedMentions: {roles: false}});
         const reason = interaction.options.getString("reason") ?? "None";
         const unpunishResult = await client.punishments.removePunishment(punishment.id, interaction.user.id, reason);
         interaction.reply(unpunishResult);
     };
-};
+}
 
 class MusicPlayer extends EventEmitter {
     constructor(client) {
+        super(client);
         this.client = client;
     }
 }
--- commands/cpuintel.js
+++ commands/cpuintel.js
@@ -3,5 +3,22 @@ module.exports = {
 	run: (client, interaction) => {
 		client.cpuCommand(client, interaction, "intel");
 	},
-	data: new SlashCommandBuilder().setName("cpuintel").setDescription("Finds a intel CPU.").addSubcommand((optt)=>optt.setName("help").setDescription("Shows you how to use the command.")).addSubcommand((optt)=>optt.setName("search").setDescription("Searches the db for the queried intel CPU.").addStringOption((opt)=>opt.setName("query").setDescription("The intel CPU to query for.").setRequired(true)))
+	data: new SlashCommandBuilder()
+		.setName("cpuintel")
+		.setDescription("Finds a intel CPU.")
+		.addSubcommand((optt)=>optt
+			.setName("help")
+			.setDescription("Shows you how to use the command."))
+		.addSubcommand((optt)=>optt
+			.setName("search").setDescription("Searches the db for the queried intel CPU.")
+			.addStringOption((opt)=>opt
+				.setName("query").setDescription("The intel CPU to query for.")
+				.setRequired(true))
+			.addStringOption(options => options
+				.setName('options')
+				.setDescription('Search options for the commands')
+				.addChoice('sl - searches and gives a list and lets you chose from it', 'sl')
+				.addChoice('s - searches and gives you a list without the option to chose', 's')
+				.addChoice('none - searches only for the specific cpu you put', 'none')
+				.setRequired(true)))
 };
