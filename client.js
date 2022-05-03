725eab37cdb9b4139da770edbd0bb2c5c6d95972
Stif <stifskere@gmail.com>
2022-05-03 22:13:19 +0200

    fucking send

--- client.js
+++ client.js
@@ -132,7 +132,7 @@ class YClient extends Client {
 
         search = search.toLowerCase().split(",")
 
-        interaction.channel.send(search.toString())
+        interaction.channel.send({content: search.toString()})
 	let matches = new client.collection();
 	let nameSearch = false;
 	let filters = [];
