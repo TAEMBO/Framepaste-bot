93814f6605f31058a148eb75dbe1c5f6c0825de2
TÆMBØ <kuneshr123@gmail.com>
2022-05-03 18:14:28 -0700

    let's try this instead

--- commands/restart.js
+++ commands/restart.js
@@ -2,7 +2,8 @@ const {SlashCommandBuilder} = require("@discordjs/builders");
 module.exports = {
   run: (client, interaction) => {
     if (!client.hasModPerms(client, interaction.member) && !interaction.member.roles.cache.has(client.config.mainServer.roles.botdeveloper)) return interaction.reply({content: `You need the <@&${interaction.guild.roles.cache.get(client.config.mainServer.roles.botdeveloper).id}> role to use this command.`, allowedMentions: {roles: false}})
-    interaction.reply("Restarting...").then(async ()=> {client.channels.cache.get(client.config.mainServer.channels.modlogs).send({content: `:exclamation:  Restart by **${interaction.user.tag}** :exclamation:`}); eval(process.exit(-1))})
+    interaction.reply("Restarting...");
+    client.channels.cache.get(client.config.mainServer.channels.modlogs).send({content: `:exclamation:  Restart by **${interaction.user.tag}** :exclamation:`}).then(async ()=> eval(process.exit(-1)))
   },
   data: new SlashCommandBuilder().setName("restart").setDescription("Restarts the bot.")
 };
