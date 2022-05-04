c87142f08489677ed234d2ed9725f0fff7c71c22
TÆMBØ <kuneshr123@gmail.com>
2022-05-03 19:41:14 -0700

    Yasser wanted it

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -16,7 +16,7 @@ module.exports = {
             {name: '🔹 Account Creation Date', value: `<t:${Math.round(new Date(member.user.createdTimestamp) / 1000)}>\n<t:${Math.round(new Date(member.user.createdTimestamp) / 1000)}:R>`},
             {name: '🔹 Join Date', value: `<t:${Math.round(new Date(member.joinedTimestamp) / 1000)}>\n<t:${Math.round(new Date(member.joinedTimestamp) / 1000)}:R>`},
             {name: '🔹 Roles', value: `${member.roles.cache.size > 1 ? member.roles.cache.filter(x => x.id !== member.guild.roles.everyone.id).sort((a, b) => b.position - a.position).map(x => x).join(member.roles.cache.size > 4 ? ' ' : '\n').slice(0, 1024) : 'None'}`, inline: true},
-            {name: '🔹 LRS msgs', value: `${await client.userLevels.getEligible(member).messages.toLocaleString('en-US')}`, inline: true})
+            {name: '🔹 Level Roles messages', value: `${await client.userLevels.getEligible(member).messages.toLocaleString('en-US')}`, inline: true})
             
             .setColor(client.config.embedColorRed)
             .setTimestamp(Date.now())
