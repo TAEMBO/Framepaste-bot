462ac4e4dda88d503aaa9e75ecb0080cbdd7dcda
TÆMBØ <kuneshr123@gmail.com>
2022-05-03 20:09:12 -0700

    forgor this

--- commands/update.js
+++ commands/update.js
@@ -17,5 +17,5 @@ module.exports = {
             return;
         }
       },
-    data: new SlashCommandBuilder().setName("update").setDescription("Pull from GitHub repository to live bot").addStringOption(options => options.setName('options').addChoice('Update and restart', 'ur').addChoice('Update', 'u').setRequired(true))
+    data: new SlashCommandBuilder().setName("update").setDescription("Pull from GitHub repository to live bot").addStringOption(opt => opt.setName('options').setDescription('Restart as well?').addChoice('Update and restart', 'ur').addChoice('Update', 'u').setRequired(true))
 };
