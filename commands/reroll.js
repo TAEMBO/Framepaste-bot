fedf1c311ed816202c77b4baa184853306e2a370
TÆMBØ <kuneshr123@gmail.com>
2022-05-05 20:05:42 -0700

    reroll that actually works

--- commands/reroll.js
+++ commands/reroll.js
@@ -3,27 +3,18 @@ const { SlashCommandBuilder } = require("@discordjs/builders");
 module.exports = {
     run: async (client, interaction) => {
         if (!client.hasModPerms(client, interaction.member)) {
-            return interaction.reply({content: `You need the <@&${interaction.guild.roles.cache.get(client.config.mainServer.roles.mod).id}> role to use this command.`, allowedMentions: {roles: false}});
+            return interaction.reply({content: `You need the <@&${client.config.mainServer.roles.mod}> role to use this command.`, allowedMentions: {roles: false}});
         }
         
-        const giveaway = client.giveawaysManager.giveaways.find(x=>x.id===interaction.options.getString("message_id"));
+        const giveaway = interaction.options.getString("message_id");
 
-        if (!giveaway) {
-            return interaction.reply(':boom: Hm. I can\'t seem to find a giveaway for `' + interaction.options.getString("message_id") + '`.');
-        }
-
-        client.giveawaysManager.reroll(giveaway.messageID)
+        client.giveawaysManager.reroll(giveaway)
             .then(() => {
                 interaction.reply('Giveaway rerolled!');
             })
             .catch((e) => {
-                if (e.startsWith(`Giveaway with interaction ID ${giveaway.messageID} has not ended.`)) {
-                    interaction.followUp('This giveaway has not ended!');
-                } else {
-                    console.error(e);
-                    interaction.followUp('An error occurred...');
-                }
+                console.log(e);
             });
     },
-    data: new SlashCommandBuilder().setDescription("Rerolls a giveaway").setName("reroll").addStringOption((opt)=>opt.setName("message_id").setDescription("The ID of the Giveaway Message.").setRequired(true))
+    data: new SlashCommandBuilder().setDescription("Rerolls a giveaway").setName("reroll").addStringOption((opt)=>opt.setName("message_id").setDescription("The ID of the giveaway message.").setRequired(true))
 }
