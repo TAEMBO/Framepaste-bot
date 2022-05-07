68dcd6a7fc80fb255923a6c6b907e05aeda7b802
TÆMBØ <kuneshr123@gmail.com>
2022-05-06 21:05:31 -0700

    major L

--- commands/say.js
+++ commands/say.js
@@ -3,7 +3,7 @@ const { SlashCommandBuilder } = require("@discordjs/builders");
 module.exports = {
 	run: (client, interaction) => {
 		interaction.reply({content: '<:Tro:945431068079697921>', ephemeral: true})
-		interaction.channel.send(interaction.options.getString("word"));
+		interaction.channel.send(interaction.options.getString("wisdom"));
 	},
 	data: new SlashCommandBuilder().setName("say").setDescription("You are the bot").addStringOption((opt)=>opt.setName("wisdom").setDescription("dorime to what computation will bring us").setRequired(true))
 };
