86ed122ac9690c8c7440ee8c50d26d2b1d863408
Stif <stifskere@gmail.com>
2022-05-07 22:42:13 +0200

    sum other stuff

--- commands/snipe.js
+++ commands/snipe.js
@@ -5,7 +5,9 @@ module.exports = {
         .setDescription("Gets the last deleted message in the channel."),
     run: (client, interaction) => {
 
-        interaction.channel.send(typeof client.snipes)
+        let values = Array.from(client.snipes.values())
+
+        interaction.channel.reply(values.toString())
 
     }
 }
