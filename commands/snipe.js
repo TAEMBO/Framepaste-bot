1a1019858eb03917f61845f0548a210d4effe49c
Stif <stifskere@gmail.com>
2022-05-08 00:50:10 +0200

    i'l continue another day

--- commands/snipe.js
+++ commands/snipe.js
@@ -1,8 +1,18 @@
 const {SlashCommandBuilder} = require("@discordjs/builders");
 module.exports = {
+    disabled: true,
     data: new SlashCommandBuilder()
         .setName("snipe")
-        .setDescription("Gets the last deleted message in the channel."),
+        .setDescription("Gets the last deleted message in the channel.")
+        .addSubcommand(get => get
+            .setName("get")
+            .setDescription("Gets the last deleted message in the channel."))
+        .addSubcommand(enable => enable
+            .setName("enable")
+            .setDescription("Enables snipe for your user."))
+        .addSubcommand(disable => disable
+            .setName("disable")
+            .setDescription("Disables snipe for your user.")),
     run: (client, interaction) => {
 
         interaction.reply(client.snipes.get(interaction.channel.id).content);
