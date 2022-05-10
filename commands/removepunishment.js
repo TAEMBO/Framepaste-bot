5a14f051e3a03777260e0538897d19a054488b7f
Yasser <73202985+Yasser-Alshinawah@users.noreply.github.com>
2022-05-10 16:08:40 -0400

    Update removepunishment.js

--- commands/removepunishment.js
+++ commands/removepunishment.js
@@ -2,7 +2,7 @@ const { SlashCommandBuilder } = require("@discordjs/builders");
 
 module.exports = {
 	run: (client, interaction) => {
-		client.unPunish(client, interaction, 'ban');
+		client.unPunish(client, interaction);
 	},
 	data: new SlashCommandBuilder().setName("unpunish").setDescription("Unpunishes a user.").addIntegerOption((opt)=>opt.setName("case_id").setDescription("The ID of the punishment to remove.").setRequired(true)).addStringOption((opt)=>opt.setName("reason").setDescription("The reason for removing the punishment.").setRequired(false))
 };
