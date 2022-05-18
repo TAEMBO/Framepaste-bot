49287996526a3126970a880f7924f43509620ce3
TÆMBØ <69138346+TAEMBO@users.noreply.github.com>
2022-05-18 12:15:44 -0700

    remove unused object

--- commands/ban.js
+++ commands/ban.js
@@ -12,6 +12,5 @@ module.exports = {
 
 		client.punish(client, interaction, 'ban');
 	},
-    data: new SlashCommandBuilder().setName("ban").setDescription("Bans a user from the guild!").addUserOption((opt)=>opt.setName("member").setDescription("The member you would like to ban.").setRequired(true)).addStringOption((opt)=>opt.setName("time").setDescription("The time for the ban.").setRequired(false)).addStringOption((opt)=>opt.setName("reason").setDescription("The reason for banning the user.").setRequired(false)),
-	category: 'Moderation'
+    data: new SlashCommandBuilder().setName("ban").setDescription("Bans a user from the guild!").addUserOption((opt)=>opt.setName("member").setDescription("The member you would like to ban.").setRequired(true)).addStringOption((opt)=>opt.setName("time").setDescription("The time for the ban.").setRequired(false)).addStringOption((opt)=>opt.setName("reason").setDescription("The reason for banning the user.").setRequired(false))
 };
