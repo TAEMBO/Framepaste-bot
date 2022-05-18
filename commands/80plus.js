d55498223657748d90a6ba1cacede19a9783e121
TÆMBØ <69138346+TAEMBO@users.noreply.github.com>
2022-05-18 12:14:05 -0700

    remove unused object

--- commands/80plus.js
+++ commands/80plus.js
@@ -3,6 +3,5 @@ module.exports = {
 	run: (client, interaction) => {
 		interaction.reply({content: 'https://www.guru3d.com/index.php?ct=articles&action=file&id=10061', allowedMentions: { repliedUser: false }});
 	},
-	data: new SlashCommandBuilder().setName("80plus").setDescription("PSU 80+ certification levels"),
-	category: 'Real Computers'
+	data: new SlashCommandBuilder().setName("80plus").setDescription("PSU 80+ certification levels")
 };
