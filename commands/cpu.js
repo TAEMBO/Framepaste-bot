2d1517a0022933b39d757d6c91a78e532cb8644b
TÆMBØ <69138346+TAEMBO@users.noreply.github.com>
2022-05-20 11:30:44 -0700

    edit cmd names and descriptions

--- commands/cpu.js
+++ commands/cpu.js
@@ -4,13 +4,13 @@ module.exports = {
 		client.cpuCommand(client, interaction, interaction.options.getString("brand"));
 	},
 	data: new SlashCommandBuilder().setName("cpu")
-		.setDescription("searches CPU specs")
+		.setDescription("Search CPU specs")
 		.addSubcommand((optt)=>optt
 			.setName("help")
 			.setDescription("Shows you how to use the command"))
 		.addSubcommand((optt)=>optt
 			.setName("search")
-			.setDescription("Searches the db for the queried CPU")
+			.setDescription("Searches CPUs")
 			.addStringOption((opt)=>opt
 				.setName("brand")
 				.setDescription("The brand to filter for")
@@ -19,13 +19,13 @@ module.exports = {
 				.setRequired(true))
 			.addStringOption((opt)=>opt
 				.setName("query")
-				.setDescription("The CPU to query for.")
+				.setDescription("The CPU(s) to search for")
 				.setRequired(true))
 			.addStringOption(options => options
 				.setName('options')
-				.setDescription('Search options for the commands')
-				.addChoice('sl - searches and gives a list', 'sl')
-				.addChoice('s - searches and gives you a list to choose from', 's')
-				.addChoice('none - searches for a matching CPU\'s specs', 'none')
+				.setDescription('Search options')
+				.addChoice('sl', 'sl')
+				.addChoice('Search a list you can choose from', 's')
+				.addChoice('Search for specific CPU specs', 'none')
 				.setRequired(true)))
 };
