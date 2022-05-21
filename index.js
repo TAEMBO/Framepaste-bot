8b7512ccb1605cbc97e355d6fb9bcc803e1a56b8
Yasser <73202985+Yasser-Alshinawah@users.noreply.github.com>
2022-05-21 15:03:23 -0400

    Update index.js

--- index.js
+++ index.js
@@ -526,8 +526,3 @@ setInterval(() => {
 client.modmailClient.on("messageCreate", message => {
 	require("./modmailMessage.js")(message, client.modmailClient, client);
 });
-
-const functionFiles = fs.readdirSync(path.resolve('./functions')).filter(file => file.endsWith(".js"));
-for (const file of functionFiles){
-	client[file.slice(0, -3)] = require(path.resolve(`./functions/${file}`));
-}
