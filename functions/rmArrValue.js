29bee44e836b8e213446f33743975417838c4034
Yasser <73202985+Yasser-Alshinawah@users.noreply.github.com>
2022-05-21 15:01:55 -0400

    Delete rmArrValue.js

--- functions/rmArrValue.js
+++
@@ -1,9 +0,0 @@
-module.exports = (array, value) => {
-        for(let i = 0; i < array.length; i++){
-            if(array[i].includes(value)){
-                array.splice(i, 1)
-                break;
-            }
-        }
-        return array;
-}
