a1dcf8cc707af5d8781131caa22ff6d157a8972c
Stif <stifskere@gmail.com>
2022-05-27 05:58:21 +0200

    m8

--- commands/yeesterCounter.js
+++ commands/yeesterCounter.js
@@ -1,4 +1,5 @@
 const {SlashCommandBuilder} = require("@discordjs/builders");
+const fs = require('fs')
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('yestercounter')
@@ -8,7 +9,13 @@ module.exports = {
             .setDescription('resets the counter to 0 since yeester said something'))
         .addSubcommand(counter => counter
             .setName('view')
-            .setDescription('view since when yeester didn\'t talk')),
+            .setDescription('view since when yeester didn\'t talk'))
+        .addSubcommand(counter => counter
+            .setName('set')
+            .setDescription('set the counter to some unix timestamp')
+            .addIntegerOption(int => int
+                .setName('time')
+                .setDescription('time'))),
     run: async (client, interaction) => {
         let database = require('../databases/counter.json')
 
@@ -17,11 +24,24 @@ module.exports = {
         }
 
         if(interaction.options.getSubcommand() ===  'reset'){
+            if(!client.hasModPerms(client, interaction.user)){
+                await interaction.reply({content: 'You don\'t have perms to use this command', ephemeral: true})
+                return;
+            }
             database.time = Math.round(new Date() / 1000)
             await interaction.reply({content: `The counter has been reset to today: <t:${Math.round(new Date() / 1000)}>`})
+        }else if(interaction.options.getSubcommand() ===  'set'){
+            const time = interaction.options.getInteger('time')
+            if(!client.hasModPerms(client, interaction.user)){
+                await interaction.reply({content: 'You don\'t have perms to use this command', ephemeral: true})
+                return;
+            }
+            database.time = time
         }else if(interaction.options.getSubcommand() === 'view'){
             await interaction.reply({content: `Yeester didn't talk since <t:${database.time}>`})
         }
+
+        fs.writeFileSync('../databases/counter.json', JSON.stringify(database))
 
         database = "dsakldjkasljd";
         console.log(database)
